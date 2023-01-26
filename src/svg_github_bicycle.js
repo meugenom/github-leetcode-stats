77ea8ac4d067908df1ee2ff5d0650cbc05c642d3
Eugen Morozov <meugenom@gmail.com>
2023-01-26 21:39:34 +0100

    small fixes, optimization loading SVG

--- src/svg_github_bicycle.js
+++ src/svg_github_bicycle.js
@@ -33,52 +33,51 @@ const generateSVG = async function (info) {
 function setInfo(info){
 	out = out +
 	`
-	<!--moved background-->	
 	<g fill="none" stroke-width="1" class="ground">
 		
-		<!-- 1 point: pullRequests-->
+		
 		<line x1="0" y1="220" x2="0" y2="165" stroke="#fee08b" stroke-width="2"/>
 		<circle cx="0" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${pullRequests.pullRequests}
 		<text x="-40" y="145" stroke-width="1" stroke="#3288bd" fill="#3288bd" class="totals">Total PRs: ${info["pullRequests"]}</text>
 		
 
-		<!-- 2 point: Issues-->
+		
 		<line x1="300" y1="220" x2="300" y2="165" stroke="#fee08b" stroke-width="2" />
 		<circle cx="300" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${issues.issues}
 		<text x="250" y="145" stroke-width="1" stroke="#3288bd" fill="#3288bd" class="totals">Closed Issues: ${info["closedIssues"]}</text>
 		
 
-		<!-- 3 point-->
+		
 		<line x1="600" y1="220" x2="600" y2="165" stroke="#fee08b" stroke-width="2" />
 		<circle cx="600" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${contributions.contributions}
 		<text x="555" y="145" stroke-width="1" stroke="#3288bd" fill="#3288bd" class="totals">Contributions: ${info["contributions"]}</text>
 		
 
-		<!-- 4 point-->
+		
 		<line x1="900" y1="220" x2="900" y2="165" stroke="#fee08b" stroke-width="2"/>
 		<circle cx="900" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${commits.commits}
 		<text x="830" y="145" stroke-width="1" stroke="#3288bd" fill="#3288bd" class="totals">Total Commits: ${info["totalCommits"]}</text>
 
 
-		<!-- 5 point-->
+		
 		<line x1="1200" y1="220" x2="1200" y2="165" stroke="#fee08b" stroke-width="2"/>
 		<circle cx="1200" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${repositories.repositories}
 		<text x="1120" y="145" stroke-width="1" stroke="#3288bd" fill="#3288bd" class="totals">Github Repositories: ${info["totalRepositories"]}</text>
 
 		
-		<!-- 6 point-->
+		
 		<line x1="1500" y1="220" x2="1500" y2="165" stroke="#fee08b" stroke-width="2" />
 		<circle cx="1500" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${followers.followers}
 		<text x="1455" y="145" stroke-width="1" stroke="#3288bd" fill="#3288bd" class="totals">Followers: ${info["followers"]}</text>
 
 
-		<!-- 7 point-->
+		
 		<line stroke="#fee08b" stroke-width="2" x1="1800" y1="220" x2="1800" y2="165" />
 		<circle cx="1800" cy="165" r="5" stroke="#fee08b" fill="#fee08b" />
 		${star.star}
